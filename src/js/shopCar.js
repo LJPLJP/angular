75085954bd260395786682b4db5708aa59f0623a
unknown <2521525349@qq.com>
2017-02-13 09:55:37 +0800

    ljp

---
+++ src/js/shopCar.js
@@ -0,0 +1,29 @@
+angular.module('myapp',[]).controller("shopCar",function($scope,$http){
+	$scope.arr = [];
+	$scope.sum = 0;
+	$http.get("http://169.254.241.177:4321/buyCar").then(function(data){
+		console.log(data);
+		$scope.arr = data.data.Data.Packages[0].Items;
+		console.log($scope.arr);
+	})
+	$scope.jian = function(s){
+		s.Qty--;
+		if(s.Qty<0){
+			s.Qty = 0;
+		}
+		sum();
+	}
+	$scope.jia = function(s){
+		s.Qty++;
+		sum();
+	}
+	sum();
+	function sum(){
+		$scope.sum = 0;
+		for(var i= 0; i<$scope.arr.length;i++){
+			$scope.sum = $scope.sum + $scope.arr[i].Qty*$scope.arr[i].Price;
+		}
+		console.log($scope.sum)
+	}
+	
+})
